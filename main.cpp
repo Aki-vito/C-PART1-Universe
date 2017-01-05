948d321b12fc0bfd13036d7211e23dc9e62f7d15
audikissa <abdullahkefahi@gmail.com>
2017-01-06 00:33:40 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
 using namespace std;
-
 class Universe {
 public:
       virtual int Size() = 0;
       virtual const char* Name() = 0;
       virtual const char* Type() = 0;
 };
-
 class Plantes : public Universe {
 int size;
 const char* name;
@@ -18,18 +16,45 @@ public:
        int Size(){return size;};
        const char* Name(){return name;};
        const char* Type(){return type;};
+};
+class Stars : public Universe{
+int size;
+const char* name;
+const char* type;
+public:
+       Stars(int _size,const char* _name,const char* _type) {size = _size;name = _name;type = "Star";}
+       int Size(){return size;};
+       const char* Name(){return name;};
+       const char* Type(){return type;};
+};
+class Black_Hole : public Universe{
+int size;
+const char* name;
+const char* type;
+public:
+       Black_Hole(int _size,const char* _name,const char* _type) {size = _size;name = _name;type = "Black Hole";};
+       int Size(){return size;};
+       const char* Name(){return name;};
+       const char* Type(){return type;};
+
+
 
 };
-
-
 int main()
 {
-Universe* uni[1];
-uni[0] = new Plantes(10,"Earth","Plant");
+Universe* uni[3];
+uni[0] = new Plantes(10,"Earth","P");
+uni[1] = new Stars(15,"Sun","S");
+uni[2] = new Black_Hole(100,"Centaurus_A","B");
 
 for(int i=0; i<3; i++) {
-    cout <<  "The size is :" << uni[i]->Size() << ", Name is : " << uni[i]->Name() << ",type is :"<<uni[i]->Type() << endl;
+    cout <<  "The Size is: " << uni[i]->Size() << ", Name is : " << uni[i]->Name() << ",Type is : "<<uni[i]->Type() << endl;
     delete uni[i];
 }
 
 }
+//*This is under-Development project to create a small encyclopidia about Universe;
+
+
+
+
